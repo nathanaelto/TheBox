2c9022ff72a00512e74e60045eab6a33c7d8ce50
Nathanael TO <nathanaelto.pro@gmail.com>
2022-12-16 18:58:10 +0100

    Add optional on settings

--- src/dto/RequestRunDTO.py
+++ src/dto/RequestRunDTO.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from dataclasses_json import dataclass_json
-from typing import List
+from typing import List, Optional
 
 from src.dto.RequestRunSettingsDTO import RequestRunSettingsDTO
 from src.dto.RequestRunStepDTO import RequestRunStepDTO
@@ -11,6 +11,6 @@ from src.dto.RequestRunStepDTO import RequestRunStepDTO
 class RequestRunDTO:
     request_id: str
     steps: List[RequestRunStepDTO]
-    settings: RequestRunSettingsDTO
+    settings: Optional[RequestRunSettingsDTO]
     files: str
 
